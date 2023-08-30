cf4feebc30fb26ae1f7c5f06c31397d0bee9e764
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-30 14:03:12 -0700

    Added Part Class

---
+++ CPW219-eCommerceSite/Models/Part.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single part for purchase
+    /// </summary>
+    public class Part
+    {
+        /// <summary>
+        /// Unique identifier for each computer part
+        /// </summary>
+        [Key]
+        public int PartId { get; set; }
+
+        /// <summary>
+        /// Name of the computer part
+        /// </summary
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Sales price of the computer part
+        /// </summary>
+        [Range(0, 10000)]
+        public double Price { get; set; }
+
+    }
+}
