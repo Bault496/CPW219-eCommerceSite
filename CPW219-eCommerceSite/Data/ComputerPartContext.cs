4d02b3676370faf5a1a564026cc6e90e736fc9de
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-30 14:18:38 -0700

    Added DB Context Class

---
+++ CPW219-eCommerceSite/Data/ComputerPartContext.cs
@@ -0,0 +1,15 @@
+﻿using CPW219_eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CPW219_eCommerceSite.Data
+{
+    public class ComputerPartContext : DbContext
+    {
+        public ComputerPartContext(DbContextOptions<ComputerPartContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Part> Parts { get; set; }
+    }
+}
