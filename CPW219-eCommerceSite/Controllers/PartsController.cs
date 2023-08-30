7940afcd85d6ff9bba2346f640463855d7c569d7
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-30 15:53:56 -0700

    Create Part functional

--- CPW219-eCommerceSite/Controllers/PartsController.cs
+++ CPW219-eCommerceSite/Controllers/PartsController.cs
@@ -1,13 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CPW219_eCommerceSite.Controllers
 {
     public class PartsController : Controller
     {
+        private readonly ComputerPartContext _context;
+        public PartsController(ComputerPartContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View(); 
+        }
+        [HttpPost]
+        public IActionResult Create(Part part)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Parts.Add(part); // prepares Insert
+                _context.SaveChanges(); // executes pending insert
+                //Show Success Message
+                ViewData["message"] = $"{part.Name} was added sucessfully!";
+                return View();
+            }
+
+            return View(part);
         }
     }
 }
