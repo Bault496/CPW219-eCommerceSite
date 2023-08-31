42f77f4d5d8d270dbf349ebaa0f5037211164526
Bart Ault <115667577+Bault496@users.noreply.github.com>
2023-08-30 20:42:46 -0700

    Added shopping cart button to part catalog

---
+++ CPW219-eCommerceSite/Controllers/CartController.cs
@@ -0,0 +1,29 @@
+﻿using CPW219_eCommerceSite.Data;
+using CPW219_eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ComputerPartContext _context;
+
+        public CartController(ComputerPartContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Add(int id)
+        {
+            Part? partToAdd = _context.Parts.Where(g => g.PartId == id).SingleOrDefault();
+
+            if(partToAdd == null)
+            {
+                TempData["Message"] = "Sorry, that part no longer is available.";
+                return RedirectToAction("Index", "Parts");
+            }
+            TempData["Message"] = "Item added to cart.";
+            return RedirectToAction("Index", "Parts");
+        }
+    }
+}
